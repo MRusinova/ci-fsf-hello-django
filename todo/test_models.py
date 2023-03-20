4b485538d753361278472cfe9ed81d8e2aebf3e5
MRusinova <rusinovamihaela@gmail.com>
2023-03-20 14:01:24 +0000

    Create test to test models equal false

---
+++ todo/test_models.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from .models import Item
+
+class TestModel(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
